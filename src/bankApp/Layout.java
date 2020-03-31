1cb4d7e00def20cff1812a542d331e25f2b842be
NuhashS <nuhash.sarwar@gmail.com>
2020-03-30 20:49:18 -0400

    Finished layout design. Finish button implementation

--- src/bankApp/Layout.java
+++ src/bankApp/Layout.java
@@ -7,27 +7,46 @@ public class Layout extends JFrame{
 
 	public Layout(String title) {
 		super(title);
-		this.setSize(700, 100);
-	 	this.setLocation(100, 100);
+		this.setSize(400, 300);
+	 	//this.setLocation(100, 100);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
 		
-		JButton button1 = new JButton("OK");
-		JButton button2 = new JButton("Cancel");
-		TextField username = new TextField("", 20);
+		JButton button1 = new JButton("Login");
+		JTextField username = new JTextField("", 15);
+		//TextPrompt usernameText = new TextPrompt("Account/Card Number", username);
+		JPasswordField password = new JPasswordField(15);
 		
-		Container mainContainer = this.getContentPane();
-		mainContainer.setLayout(new BorderLayout(500,500));
-		mainContainer.setBackground(Color.YELLOW);
+		Container mainPanel = this.getContentPane();
+		mainPanel.setLayout(new GridBagLayout());
 		
-		JPanel topPanel = new JPanel();
-		topPanel.setLayout(new FlowLayout());
-		topPanel.add(username);
-		mainContainer.add(topPanel, BorderLayout.NORTH);
+		GridBagConstraints usernameTextfieldConstraints = new GridBagConstraints();
+		//usernameTextfield.fill = GridBagConstraints.BOTH;
+		//usernameTextfield.weightx = 1;
+		usernameTextfieldConstraints.gridx = 0;
+		usernameTextfieldConstraints.gridy = 0;
+		usernameTextfieldConstraints.ipadx = 5;
+		usernameTextfieldConstraints.ipady = 5;
+		usernameTextfieldConstraints.insets = new Insets(10, 50, 10, 50);
+		mainPanel.add(username, usernameTextfieldConstraints);
 		
-		JPanel bottomPanel = new JPanel();
-		bottomPanel.setLayout(new FlowLayout(1,0,5));
-		bottomPanel.add(button1);
-		bottomPanel.add(button2);
-		mainContainer.add(bottomPanel, BorderLayout.SOUTH);
+		GridBagConstraints passwordTextfieldConstraints = new GridBagConstraints();
+		passwordTextfieldConstraints.weightx = 0.5;
+		passwordTextfieldConstraints.gridx = 0;
+		passwordTextfieldConstraints.gridy = 1;
+		passwordTextfieldConstraints.ipadx = 5;
+		passwordTextfieldConstraints.ipady = 5;
+		passwordTextfieldConstraints.insets = new Insets(10, 0, 10, 0);
+		mainPanel.add(password, passwordTextfieldConstraints);
+		
+		GridBagConstraints button1Constraints = new GridBagConstraints();
+		button1Constraints.weightx = 0.2;
+		button1Constraints.gridx = 0;
+		button1Constraints.gridy = 2;
+		button1Constraints.ipadx = 5;
+		button1Constraints.ipady = 5;
+		button1Constraints.insets = new Insets(10, 0, 10, 0);
+		mainPanel.add(button1, button1Constraints);
+		
 	}
 }
